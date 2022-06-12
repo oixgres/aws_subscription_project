0370944f330a49781a18e1e1a5aa9c0f849520b9
oixgres <a1252819@uabc.edu.mx>
2022-06-12 12:49:36 -0700

    Adding dynamodb table

--- subscribe_user/app.py
+++ subscribe_user/app.py
@@ -1,8 +1,14 @@
 import json
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('subscriptions')
 
 def lambda_handler(event, context):
+
+    print(table.creation_date_time)
+
     print(event)
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
     }

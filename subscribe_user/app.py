0bbc5d4d6a800ddf86d65c7ede74f1b2860b6452
oixgres <a1252819@uabc.edu.mx>
2022-06-12 20:47:40 -0700

    Finished with program, gonna give some design to the frontend

--- subscribe_user/app.py
+++ subscribe_user/app.py
@@ -1,5 +1,5 @@
 import json
-from urllib import response
+import re
 import boto3
 
 dynamodb = boto3.resource('dynamodb')
@@ -7,26 +7,30 @@ table = dynamodb.Table('subscriptions')
 
 def lambda_handler(event, context):
 
-    # Check if the user is already subscribed
-    email = table.get_item(
-        Key={
-            'email': event['email']
+    try:
+        email=event['queryStringParameters']['email']
+
+        # Check if email has format (regex)
+        if not re.match(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email):
+            res={'res':'Email syntax is wrong', 'error':'error'}
+        else:
+            try:
+                table.put_item(
+                    Item={
+                        'email':email
+                    },
+                    ConditionExpression="attribute_not_exists(email)"
+                )
+                res={'res': 'User has been subscribed'}
+            except:
+                res={'res': 'User was already subscribed', 'error':'error'}
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps(res)
         }
-    )
-
-    # If the user is already subscribed, finish
-    if email:
-        res='User are already subscribed'
-    # Else, subscribe the user
-    else:
-        table.put_item(
-            Item={
-                'email': event['email']
-            }
-        )
-        res = 'User subscribed'
-
-    return {
-        'statusCode': 200,
-        'body': json.dumps(res)
-    }
+    except:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'res':'Request failed', 'error':'error'})
+        }
